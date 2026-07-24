15c06d8dfdaafa4f7657ca6e7ea4c8ec8d81512f
Omkar Potphode <stackcraftlabs@gmail.com>
2026-07-24 12:23:27 +0530

    added the meta pixel to the code

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Hind } from "next/font/google";
+import Script from "next/script";
 import "./globals.css";
 
 const hind = Hind({
@@ -32,6 +33,29 @@ export default function RootLayout({
       className={`${hind.variable} antialiased scroll-smooth`}
     >
       <body className="min-h-screen flex flex-col font-sans text-text bg-bg selection:bg-brand-magenta selection:text-white">
+        <Script id="meta-pixel" strategy="afterInteractive">
+          {`
+            !function(f,b,e,v,n,t,s)
+            {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+            n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+            if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+            n.queue=[];t=b.createElement(e);t.async=!0;
+            t.src=v;s=b.getElementsByTagName(e)[0];
+            s.parentNode.insertBefore(t,s)}(window, document,'script',
+            'https://connect.facebook.net/en_US/fbevents.js');
+            fbq('init', '1263526262482187');
+            fbq('track', 'PageView');
+          `}
+        </Script>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: "none" }}
+            src="https://www.facebook.com/tr?id=1263526262482187&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         {children}
       </body>
     </html>
